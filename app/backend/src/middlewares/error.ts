194ea0e42f25c52b151fd68526e17d46ea54b555
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2022-04-06 19:51:23 -0300

    Refactor: Change Entrypoint to CMD and remove comments

--- app/backend/src/middlewares/error.ts
+++ app/backend/src/middlewares/error.ts
@@ -2,7 +2,6 @@ import { Errback, NextFunction, Request, Response } from 'express';
 import { HTTPStatusCode } from '../helpers';
 
 export default function ErrorMiddleware(err: Errback, _req: Request, res: Response, _next: NextFunction): void {
-  console.error(err)
   return res.status(HTTPStatusCode.InternalServerError).json({
     message: 'Internal server error',
   }).end();
