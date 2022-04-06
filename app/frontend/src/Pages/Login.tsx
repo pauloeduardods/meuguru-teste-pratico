fc519c177915e7a752bb566d3a7436d7f07f02c7
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2022-04-06 19:53:08 -0300

    Feat: Add login page

---
+++ app/frontend/src/Pages/Login.tsx
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+import { LockClosedIcon } from '@heroicons/react/solid';
+import logo from '../Images/image.svg';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isButtonEnabled, setIsButtonEnabled] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault();
+      const result = await fetch(`${process.env.REACT_APP_BASE_URL}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      const { token } = await result.json();
+      localStorage.setItem('token', token);
+    } catch (error) {
+      setIsError(true);
+    }
+  };
+
+  return (
+    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-meuguru h-vh-100">
+      <div className="max-w-md w-full space-y-8 sm:border-solid sm:border-slate-700 bg-slate-900 sm:p-3 sm:border-2 sm:rounded-xl sm:shadow-md">
+        <div className="relative mt-3">
+          <img
+            className="mx-auto h-12 w-auto"
+            src={logo}
+            alt="Logo"
+          />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-100">Login</h2>
+        </div>
+        <form className="mt-8 space-y-10" onSubmit={handleSubmit}>
+          <input type="hidden" name="remember" defaultValue="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="input-email" className="sr-only">
+                E-mail
+              </label>
+              <input
+                id="input-email"
+                data-testid="input-email"
+                name="email"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+                type="email"
+                autoComplete="email"
+                value={email}
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-slate-300 focus:border-slate-500 focus:z-10 sm:text-sm"
+                placeholder="E-mail"
+              />
+            </div>
+            <div>
+              <label htmlFor="input-password" className="sr-only">
+                Senha
+              </label>
+              <input
+                id="input-password"
+                name="password"
+                type="password"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+                autoComplete="password"
+                value={password}
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-slate-300 focus:border-slate-500 focus:z-10 sm:text-sm"
+                placeholder="Senha"
+              />
+            </div>
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-700 hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600 disabled:opacity-60 mb-1"
+              data-testid="btn-play"
+              disabled={isButtonEnabled}
+            >
+              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                <LockClosedIcon className="h-5 w-5 text-violet-200 group-hover:text-violet-100" aria-hidden="true" />
+              </span>
+              Entrar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
