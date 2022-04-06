a6cc19fedba295a396a60ea1dde22fb76647ca42
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2022-04-06 19:52:24 -0300

    Feat: Add react in frontend

---
+++ app/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './Pages/Login';
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ app/frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        meuguru: '#2a004f',
+      },
+      spacing: {
+        'vh-100': '100vh',
+      },
+    },
+  },
+  plugins: [],
+};
