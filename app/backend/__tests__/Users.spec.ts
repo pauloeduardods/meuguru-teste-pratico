811c48c77895920bcf217d57becbb5b289ee2706
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2022-04-07 18:22:55 -0300

    Tests: Add test to GET /users

--- app/backend/__tests__/Users.spec.ts
+++ app/backend/__tests__/Users.spec.ts
@@ -1,7 +1,7 @@
 import ChildProcess from 'child_process';
 import Axios from 'axios';
 import QueryString from 'qs';
-import { PrismaClient } from '@prisma/client';
+import { Page1Users, Page2Users, PauloEduardoUser } from './mocks/User.mock';
 
 const AxiosClient = Axios.create({
   baseURL: 'http://localhost:3001',
@@ -23,29 +23,62 @@ function exec(command: string) {
   });
 }
 
-const usersMock = [
-  { id: 1, email: 'pauloeduardods@email.com', name: 'Paulo Eduardo' },
-  { id: 2, email: 'nilu@prisma.io', name: 'Nilu' },
-  { id: 3, email: 'mahmoud@prisma.io', name: 'Mahmoud' }
-];
-
-const prisma = new PrismaClient();
-
 describe('Users', () => {
   jest.setTimeout(30000);
   beforeEach(async () => {
-    await prisma.user.deleteMany({});
-    await exec('npm run migrate');
-    await exec('npm run seed');
+    await exec('npm run migrate:undo');
   });
-  // afterEach(async () => {
-  //   await prisma.user.deleteMany({});
-  // });
   describe('GET /users', () => {
     it('should return 10 users with no query params', async () => {
       const result = await AxiosClient.get('/users');
-      console.log(result.data)
-      expect(true).toEqual(result.data);
+      expect(result.status).toBe(200);
+      expect(Page1Users).toEqual(result.data);
+    });
+    it('Should return the page 1 when queryParams page=1', async () => {
+      const result = await AxiosClient.get('/users', { params: { page: 1 } });
+      expect(result.status).toBe(200);
+      expect(result.data).toEqual(Page1Users);
+    });
+
+    it('Should return the page 2 when queryParams page=2', async () => {
+      const result = await AxiosClient.get('/users', { params: { page: 2 } });
+      expect(result.status).toBe(200);
+      expect(result.data).toEqual(Page2Users);
+    });
+
+    it('Should return the page 1 when queryParams page=0', async () => {
+      const result = await AxiosClient.get('/users', { params: { page: 0 } });
+      expect(result.status).toBe(200);
+      expect(result.data).toEqual(Page1Users);
+    });
+    it('Should return the page 1 when queryParams page=-200', async () => {
+      const result = await AxiosClient.get('/users', { params: { page: -200 } });
+      expect(result.status).toBe(200);
+      expect(result.data).toEqual(Page1Users);
+    });
+
+    it('Should return the user Paulo Eduardo when queryParams name=Paulo Eduardo', async () => {
+      const result = await AxiosClient.get('/users', { params: { name: 'Paulo Eduardo' } });
+      expect(result.status).toBe(200);
+      expect(result.data).toEqual([PauloEduardoUser]);
+    });
+
+    it('Should return the user Paulo Eduardo when queryParams email=pauloeduardods@email.com', async () => {
+      const result = await AxiosClient.get('/users', { params: { email: 'pauloeduardods@email.com' } });
+      expect(result.status).toBe(200);
+      expect(result.data).toEqual([PauloEduardoUser]);
+    });
+
+    it('Should return the user Paulo Eduardo when queryParams email=pauloeduardods@email.com && name=Paulo Eduardo', async () => {
+      const result = await AxiosClient.get('/users', { params: { email: 'pauloeduardods@email.com', name: 'Paulo Eduardo' } });
+      expect(result.status).toBe(200);
+      expect(result.data).toEqual([PauloEduardoUser]);
+    });
+
+    it('Should return the 404 Status and empty array if dont find any user', async () => {
+      const result = await AxiosClient.get('/users', { params: { email: 'xaulinmatadordeporco@email.com', name: 'Xaulin Matador de Porco' } }).catch(error => error);
+      expect(result.response.status).toBe(404);
+      expect(result.response.data).toEqual([]);
     });
   });
 });
---
+++ app/backend/__tests__/mocks/User.mock.ts
@@ -0,0 +1,96 @@
+export const PauloEduardoUser = {
+  "id": 1,
+  "email": "pauloeduardods@email.com",
+  "name": "Paulo Eduardo"
+}
+
+export const Page1Users = [
+  {
+    "id": 1,
+    "email": "pauloeduardods@email.com",
+    "name": "Paulo Eduardo"
+  },
+  {
+    "id": 2,
+    "email": "nilu@prisma.io",
+    "name": "Nilu"
+  },
+  {
+    "id": 3,
+    "email": "mahmoud@prisma.io",
+    "name": "Mahmoud"
+  },
+  {
+    "id": 4,
+    "email": "caetano@email.com",
+    "name": "Caetano"
+  },
+  {
+    "id": 5,
+    "email": "cabecabranca@email.com",
+    "name": "Valdir"
+  },
+  {
+    "id": 6,
+    "email": "cabeeeecabranca@email.com",
+    "name": "Luis"
+  },
+  {
+    "id": 7,
+    "email": "luiscomz@email.com",
+    "name": "Luis com z"
+  },
+  {
+    "id": 8,
+    "email": "luizcoms@email.com",
+    "name": "Luiz com s"
+  },
+  {
+    "id": 9,
+    "email": "olator@email.com",
+    "name": "Ice"
+  },
+  {
+    "id": 10,
+    "email": "anaca@email.com",
+    "name": "Anaca"
+  }
+]
+
+export const Page2Users = [
+  {
+    "id": 11,
+    "email": "anacaaaaaaaaaaaa@email.com",
+    "name": "Anacaaa"
+  },
+  {
+    "id": 12,
+    "email": "anacaaaaaaaa@email.com",
+    "name": "Anacaa"
+  },
+  {
+    "id": 13,
+    "email": "anacaaa@email.com",
+    "name": "Anacaaaa"
+  },
+  {
+    "id": 14,
+    "email": "anacaaaaaaa@email.com",
+    "name": "Anacaa"
+  },
+  {
+    "id": 15,
+    "email": "lucas@email.com",
+    "name": "Lucas"
+  },
+  {
+    "id": 16,
+    "email": "thiago@email.com",
+    "name": "thiago"
+  },
+  {
+    "id": 17,
+    "email": "claudio@email.com",
+    "name": "claudio"
+  }
+]
