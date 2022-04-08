f3eb94b2c6520e21626d2a6bf55319bc9478710b
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2022-04-07 22:09:27 -0300

    Add pagination

--- app/frontend/src/Pages/Home.tsx
+++ app/frontend/src/Pages/Home.tsx
@@ -8,7 +8,6 @@ function Home() {
   const [users, setUsers] = useState<IUser[]>([]);
   const [name, setName] = useState<string>();
   const [email, setEmail] = useState<string>();
-  // eslint-disable-next-line no-unused-vars
   const [page, setPage] = useState<number>(1);
 
   useEffect(() => {
@@ -27,7 +26,7 @@ function Home() {
   }
 
   return (
-    <div>
+    <div className="h-vh-100 bg-meuguru">
       <Header />
       <div className="flex flex-col mt-10">
         <div className="-my-2 overflow-x-auto">
@@ -36,35 +35,35 @@ function Home() {
               <input type="text" placeholder="Nome" value={name} onChange={({ target: { value } }) => setName(value)} />
               <input type="text" placeholder="Email" value={email} onChange={({ target: { value } }) => setEmail(value)} />
             </div>
-            <div className="shadow overflow-hidden border-b border-t border-gray-300 sm:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
+            <div className="shadow overflow-hidden border-b border-t border-slate-700 sm:rounded-lg">
+              <table className="min-w-full divide-y divide-slate-800">
+                <thead className="bg-slate-900">
                   <tr>
                     <th className="sm:px-1 md:px-2 lg:px-3 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Id</th>
                     <th className="sm:px-1 md:px-2 lg:px-3 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Nome</th>
                     <th className="sm:px-1 md:px-2 lg:px-3 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider">Email</th>
                   </tr>
                 </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
+                <tbody className="bg-slate-900 divide-y divide-gray-800">
                   {users?.map(({ id, email: curEmail, name: curName }) => (
                     <tr key={id}>
                       <td className="sm:px-1 md:px-2 lg:px-3 py-4 whitespace-nowrap">
                         <div className="flex items-center place-content-center">
-                          <div className="text-sm font-medium text-gray-900 truncate sm:max-w-xss xl:max-w-mdd">
+                          <div className="text-sm font-medium text-gray-50 truncate sm:max-w-xss xl:max-w-mdd">
                             {id}
                           </div>
                         </div>
                       </td>
                       <td className="sm:px-1 md:px-2 lg:px-3 py-4 whitespace-nowrap">
                         <div className="flex items-center place-content-center">
-                          <div className="text-sm font-medium text-gray-900">
+                          <div className="text-sm font-medium text-gray-50">
                             {curName}
                           </div>
                         </div>
                       </td>
                       <td className="sm:px-1 md:px-2 lg:px-3 py-4 whitespace-nowrap">
                         <div className="flex items-center place-content-center">
-                          <div className="text-sm font-medium text-gray-900">
+                          <div className="text-sm font-medium text-gray-50">
                             {curEmail}
                           </div>
                         </div>
@@ -73,6 +72,25 @@ function Home() {
                   ))}
                 </tbody>
               </table>
+            </div>
+            <div className="mt-4 w-full flex justify-center sm:justify-end">
+              <button
+                type="button"
+                onClick={() => setPage(page - 1)}
+                disabled={page <= 1}
+                className="my-2 mx-2 sm:my-0 flex items-center justify-center px-10 py-1 border border-transparent rounded-md shadow-sm text-base font-medium text-gray-900 bg-green-400 hover:bg-green-500 disabled:bg-green-800"
+              >
+                Anterior
+              </button>
+              <span className="mx-3 text-gray-200">{page}</span>
+              <button
+                type="button"
+                onClick={() => setPage(page + 1)}
+                disabled={users?.length < 10}
+                className="my-2 mx-2 sm:my-0 flex items-center justify-center px-10 py-1 border border-transparent rounded-md shadow-sm text-base font-medium text-gray-900 bg-green-400 hover:bg-green-500 disabled:bg-green-800"
+              >
+                Próximo
+              </button>
             </div>
           </div>
         </div>
